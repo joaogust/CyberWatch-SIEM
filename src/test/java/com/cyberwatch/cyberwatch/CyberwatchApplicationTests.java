bd27c274fe104fc532dc6fcfeb21b169faa73d88
joaogust <joaogustavodossantoss@gmail.com>
2026-06-03 15:45:36 -0300

    Criação do projeto SpringBoot e também  criação e configuração do Banco de Dados via Render

---
+++ src/test/java/com/cyberwatch/cyberwatch/CyberwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberwatch.cyberwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
